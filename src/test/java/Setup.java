f14c6791e65f8acb3015394bdf3355532364f52d
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2019-11-25 17:51:43 +0100

    Add a first Setup test

--- src/test/java/Setup.java
+++ src/test/java/Setup.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Setup {
 
-    @Test
-    public void isGood() {
-        assertTrue(true);
-    }
+  @Test
+  public void isGood() {
+    assertTrue(true);
+  }
 }
