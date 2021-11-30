f757f0c38ef0420fd3eaa69329ad1797ed386c29
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2021-11-30 19:46:52 +0100

    Add .NET C# solution stub

---
+++ csharp/KataSupermarket.Tests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace KataSupermarket.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
