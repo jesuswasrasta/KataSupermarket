e81ca74b0b92d7530e6f016bf62bd174e92ed336
Roberto Federiconi <r.federiconi@namirial.com>
2022-01-24 18:13:23 +0100

    wip

--- typescript/__tests__/main.test.ts
+++ typescript/__tests__/main.test.ts
@@ -1,42 +1,12 @@
-import { Delays, greeter } from '../src/main';
+import { checkout, checkoutFruit, Fruit } from '../src/main';
 
-describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
-
-  let timeoutSpy: jest.SpyInstance;
-
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
-
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
+describe('minimal checkout', () => {
+  it('When I checkout an apple, the system charges 50 cents', () => {
+    const value = checkout(1);
+    expect(value).toEqual(50);
   });
-
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
+  it('When I checkout 1 pear, the system charges 30 cents', () => {
+    const value = checkoutFruit(Fruit.PEAR, 1);
+    expect(value).toEqual(30);
   });
 });
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,32 +1,22 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+const apple = 50;
+const pear = 30;
+
+export enum Fruit {
+  APPLE = 'APPLE',
+  PEAR = 'PEAR',
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
+export function checkout(quantity: number): number {
+  return quantity * apple;
 }
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
+export function checkoutFruit(fruit: Fruit, quantity: number): number {
+  switch (fruit) {
+    case Fruit.APPLE:
+      return checkout(quantity);
+    case Fruit.PEAR:
+      return quantity * pear;
+    default:
+      throw new Error('Undefined');
+  }
 }
